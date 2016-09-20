e389fb94820fe4c929c2cc787d20253ca497c015
Yaroslav Panasenko <yaroslaw.panasencko@yandex.ua>
2016-09-20 16:19:41 +0300

    Added ability to rating request change states

--- library/src/main/java/com/volen/ratingrequest/RatingRequestView.java
+++ library/src/main/java/com/volen/ratingrequest/RatingRequestView.java
@@ -1,15 +1,30 @@
 package com.volen.ratingrequest;
 
 import android.content.Context;
+import android.media.Rating;
+import android.support.annotation.IntDef;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 public class RatingRequestView extends FrameLayout {
     private View mainView;
     private RatingRequestDialogItem nudgeView;
     private RatingRequestDialogItem ratingView;
     private RatingRequestDialogItem feedbackView;
+
+    @IntDef({ NUDGE, FEEDBACK, RATING })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface State {}
+
+    public static final int NUDGE = 1;
+    public static final int RATING = 2;
+    public static final int FEEDBACK = 3;
+
+    private @RatingRequestView.State int state;
 
     public RatingRequestView(Context context) {
         super(context);
@@ -25,7 +40,10 @@ public class RatingRequestView extends FrameLayout {
     private void init(){
         mainView = inflate(getContext(), R.layout.rating_request_dialog_item, this);
 
+        state = NUDGE;
+
         initUi();
+        initActions();
     }
 
     private void initUi(){
@@ -33,5 +51,46 @@ public class RatingRequestView extends FrameLayout {
         ratingView = (RatingRequestDialogItem)findViewById(R.id.rating);
         feedbackView = (RatingRequestDialogItem)findViewById(R.id.feedback);
     }
+
+    private void initActions(){
+        nudgeView.setOnDecisionListener(new RatingRequestDialogItem.OnDecisionListener() {
+            @Override
+            public void onAccept() {
+                setState(RATING);
+            }
+
+            @Override
+            public void onDecline() {
+                setState(FEEDBACK);
+            }
+        });
+    }
     //endregion Init
+
+    public int getState(){
+        return state;
+    }
+
+    public void setState(@State int state){
+        if (this.state == state)
+            return;
+
+        nudgeView.hide();
+        feedbackView.hide();
+        ratingView.hide();
+
+        switch (state){
+            case NUDGE:
+                nudgeView.show();
+                break;
+            case FEEDBACK:
+                feedbackView.show();
+                break;
+            case RATING:
+                ratingView.show();
+                break;
+        }
+
+        this.state = state;
+    }
 }
